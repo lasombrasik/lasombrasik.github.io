d37c026687510a615f8bf574e545b6dd3bc829a0
Kyrychuk Volodymyr <lasombrasik@gmail.com>
2023-06-01 02:07:34 +0300

    database change

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::defaultStringLength(191);
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email', 999)->primary();
+            $table->string('email', 128)->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
