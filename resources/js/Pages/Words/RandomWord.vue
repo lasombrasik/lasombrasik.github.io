b150426f4b90b93c43227e76274a8bfd83244dd9
Kyrychuk Volodymyr <lasombrasik@gmail.com>
2023-06-06 00:35:42 +0200

    make a speech

--- resources/js/Pages/Words/RandomWord.vue
+++ resources/js/Pages/Words/RandomWord.vue
@@ -4,19 +4,37 @@
     <AuthenticatedLayout>
         <div class="flex justify-center h-fit">
             <div class="h-1/4 w-full block p-5">
-                <h2 class="font-bold text-white text-center sm:w-1/3 mx-auto bg-gray-600 text-3xl">{{ 'Word:' }}</h2><br>
+                <h2 class="font-bold text-white text-center sm:w-1/3 mx-auto bg-gray-600 text-3xl relative">
+                    {{ 'Word:' }}
+                    <button
+                        class="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-600 hover:text-gray-400 text-white font-bold pr-4 rounded text-center"
+                        @click="mute()"
+                    >
+                        <i v-if="soundOn" class="fas fa-volume-up fa-xs"></i>
+                        <i v-else class="fas fa-volume-mute fa-xs"></i>
+                    </button>
+                </h2><br>
                 <div class="text-center">
-                    <p class="text-3xl font-bold p-12">
+                    <div class="text-3xl font-bold p-12 inline-flex">
                         {{ randomWord }}
-                    </p>
-                    <p class="text-3xl text-green-600">
+                        <button
+                            class="transform -translate-y-1/2 text-blue-400 hover:text-blue-600 text-white font-bold rounded"
+                            @click="speak(randomWord)"
+                        >
+                            <i class="fa fa-play-circle text-xs"></i>
+                        </button>
+                    </div>
+                    <div class="text-3xl text-green-600">
                         {{ showTranslate ? translation : translationDots }}
-                    </p>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="flex justify-center">
-            <button class="w-120 bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded text-center" @click="showTranslation">
+            <button
+                class="w-120 bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded text-center"
+                @click="showTranslation"
+            >
                 {{ button }}
             </button>
         </div>
@@ -32,7 +50,8 @@ export default {
             randomWord: '',
             translation: '',
             button: 'Show Translation',
-            translationDots: ''
+            translationDots: '',
+            soundOn: true
         }
     },
     mounted() {
@@ -50,20 +69,47 @@ export default {
             }
         },
         startRandomWord() {
-            const randomIndex = Math.floor(Math.random() * this.words.length);
-            this.randomWord = this.words[randomIndex].article ? this.words[randomIndex].article + ' ' + this.words[randomIndex].word : this.words[randomIndex].word;
-            this.translation = this.words[randomIndex].translation;
-            this.displayDots();
+            this.createRandomWord();
+            if (this.soundOn) {
+                this.speak(this.randomWord);
+            }
         },
         generateRandomWord() {
             const randomIndex = Math.floor(Math.random() * this.words.length);
             this.randomWord = this.words[randomIndex].article ? this.words[randomIndex].article + ' ' + this.words[randomIndex].word : this.words[randomIndex].word;
             this.translation = this.words[randomIndex].translation;
             this.displayDots();
+
+            if (this.soundOn) {
+                this.speak(this.randomWord);
+            }
+
             this.showTranslate = false;
         },
         displayDots() {
             this.translationDots = this.translation.replace(/[^\s,.\p{P}]/gu, "*");
+        },
+        speak(text) {
+            window.speechSynthesis.cancel();
+            let utterance = new SpeechSynthesisUtterance(text);
+            utterance.lang = 'de-DE';
+            utterance.pitch = 0.9;
+            utterance.rate = 0.8;
+            utterance.volume = 0.5;
+            window.speechSynthesis.speak(utterance);
+        },
+        mute() {
+            this.soundOn = !this.soundOn;
+
+            if (!this.soundOn) {
+                window.speechSynthesis.cancel();
+            }
+        },
+        createRandomWord() {
+            const randomIndex = Math.floor(Math.random() * this.words.length);
+            this.randomWord = this.words[randomIndex].article ? this.words[randomIndex].article + ' ' + this.words[randomIndex].word : this.words[randomIndex].word;
+            this.translation = this.words[randomIndex].translation;
+            this.displayDots();
         }
     }
 }
@@ -72,6 +118,7 @@ export default {
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 defineProps(['words']);
 </script>
