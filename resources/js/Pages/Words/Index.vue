96074677ac06221f45fd5d788e816d34dec3dcd8
Kyrychuk Volodymyr <lasombrasik@gmail.com>
2023-06-06 01:51:36 +0200

    added convenient addition of words from the keyboard

--- resources/js/Pages/Words/Index.vue
+++ resources/js/Pages/Words/Index.vue
@@ -25,7 +25,7 @@ const form = useForm({
                         id="article-input"
                         type="text"
                         v-model="form.article"
-                        @input="checkInputLength()"
+                        @input="checkInputArticle()"
                         placeholder="Article"
                         maxlength="3"
                         class="block w-1/4 mr-2 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm text-center"
@@ -34,6 +34,7 @@ const form = useForm({
                         id="word-input"
                         type="text"
                         v-model="form.word"
+                        @keyup.enter="checkInputWord()"
                         placeholder="Word"
                         class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
                     />
@@ -42,12 +43,19 @@ const form = useForm({
                     id="translation-input"
                     type="text"
                     v-model="form.translation"
+                    @keyup.enter="checkInputTranslation()"
                     placeholder="Translation"
                     class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
                 />
-                <InputError :message="form.errors.word" class="mt-2" />
+                <InputError
+                    :message="form.errors.word"
+                    class="mt-2"
+                />
                 <div class="text-center">
-                    <PrimaryButton class="mt-4">
+                    <PrimaryButton
+                        id="submit-button"
+                        class="mt-4"
+                    >
                         {{ 'Add Word' }}
                     </PrimaryButton>
                 </div>
@@ -67,7 +75,7 @@ const form = useForm({
 export default {
     name: "Index",
     methods: {
-         checkInputLength() {
+         checkInputArticle() {
             let input = document.getElementById("article-input");
             let value = input.value;
 
@@ -82,6 +90,12 @@ export default {
                     nextInput.focus();
                 }
             }
+        },
+        checkInputWord() {
+            document.getElementById("translation-input").focus();
+        },
+        checkInputTranslation() {
+            document.getElementById("submit-button").focus();
         }
     }
 }
